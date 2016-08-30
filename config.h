ac569014d7043b5bec8de6954c6616c0144ff67f
Wojciech Kozlowski <wojciech.kozlowski@vivaldi.net>
2016-08-30 22:27:57 +0100

    Added the keybinding Mod-s for tsurf

--- config.h
+++ config.h
@@ -63,12 +63,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[]  = { "lock", NULL };
+static const char *surfcmd[]  = { "tsurf", NULL };
 
 static Key keys[] = {
 	/* modifier                     key                function        argument */
 	{ MODKEY,                       XK_p,              spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,         spawn,          {.v = termcmd } },
 	{ ControlMask|Mod1Mask,         XK_l,              spawn,          {.v = lockcmd } },
+	{ MODKEY,                       XK_s,              spawn,          {.v = surfcmd } },
 	{ MODKEY,                       XK_b,              togglebar,      {0} },
 	{ MODKEY,                       XK_k,              focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_l,              focusstack,     {.i = -1 } },
