e39dc2ca42d3081f7b3eb3c8547e497ea7cde747
Wojciech Kozlowski <wojciech.kozlowski@vivaldi.net>
2016-09-10 22:39:27 +0100

    Removed reddit icon as I'm not going to use RTV in the end

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static const char *colors[SchemeLast][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
